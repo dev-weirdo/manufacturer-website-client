cd75b0ecc34eb25387c02e76ef2f0cf3479ad17d
dev-weirdo <dev_weirdo@protonmail.com>
2022-05-22 21:08:24 +0600

    Header and Hero added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Tools from "./components/Tools";
+
 function App() {
   return (
     <div>
+      <Header></Header>
+      <Hero></Hero>
+      <Tools></Tools>
     </div>
   );
 }
