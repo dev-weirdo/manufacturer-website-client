8165c825eea79604379bd6d47c17b4424e5c4d0b
dev-weirdo <dev_weirdo@protonmail.com>
2022-05-26 00:59:14 +0600

    added button disabled on condition

--- src/components/Dashboard/AddReview.js
+++ src/components/Dashboard/AddReview.js
@@ -3,7 +3,18 @@ import React from 'react';
 const AddReview = () => {
     return (
         <div>
-            <h1>Review</h1>
+            <div className="form-control">
+                <label className="input-group">
+                    <span>Rating</span>
+                    <input type="number" placeholder="info@site.com" className="input input-bordered" />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className="input-group">
+                    <span>Review</span>
+                    <textarea type="text" placeholder="info@site.com" className="input input-bordered" />
+                </label>
+            </div>
         </div>
     );
 };
